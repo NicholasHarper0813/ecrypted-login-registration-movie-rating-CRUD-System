faf747b9e7d165ac9313d61b768fd31600d76a4d
nicholas harper <nicholasharper000@gmail.com>
2024-12-15 07:54:37 -0800

    Update User.php

--- Classes/User.php
+++ Classes/User.php
@@ -1,40 +1,54 @@
 <?php
-class User {
+class User 
+{
     private $_db, $_data, $_sessionName, $_cookieName, $_isLoggedIn;
 
-    public function __construct($user = null) {
+    public function __construct($user = null) 
+    {
         $this->_db = DB::getInstance();
 
         $this->_sessionName = Config::get('session/session_name');
         $this->_cookieName = Config::get('remember-me/cookie_name');
 
-        if(!$user) {
-            if(Session::exists($this->_sessionName)) {
+        if(!$user) 
+        {
+            if(Session::exists($this->_sessionName))
+            {
                 $user = Session::get($this->_sessionName);
                 
-                if($this->find($user)) {
+                if($this->find($user))
+                {
                     $this->_isLoggedIn = true;
-                } else {
+                }
+                else 
+                {
                     // process logout
                 }
             }
-        } else {
+        }
+        else 
+        {
             $this->find($user);
         }
     }
 
-    public function create($fields = array()) {
-        if(!$this->_db->insert('users', $fields)) {
+    public function create($fields = array()) 
+    {
+        if(!$this->_db->insert('users', $fields))
+        {
             throw new Exception('There was a problem creating an account.');
         }
     }
 
-    public function find($user = null) {
-        if($user) {
+    public function find($user = null)
+    {
+        if($user)
+        {
             $field = (is_numeric($user)) ? 'id' : 'email';
             $data = $this->_db->get('users', array($field, '=', $user));
 
-            if($data->count()) {
+            if($data->count()) 
+            {
                 $this->_data = $data->first();
                 return true;
             }
@@ -42,27 +56,37 @@ class User {
         return false;
     }
 
-    public function login($email = null, $password = null, $remember = false) {
+    public function login($email = null, $password = null, $remember = false)
+    {
 
-        if(!$email && !$password && $this->exists()) {
+        if(!$email && !$password && $this->exists()) 
+        {
             Session::put($this->_sessionName, $this->data()->id);
-        } else {
+        } 
+        else 
+        {
             $user = $this->find($email);
 
-            if($user) {
-                if($this->data()->Password === Hash::make($password, $this->data()->salt)) {
+            if($user) 
+            {
+                if($this->data()->Password === Hash::make($password, $this->data()->salt)) 
+                {
                     Session::put($this->_sessionName, $this->data()->id);
 
-                    if($remember) {
+                    if($remember)
+                    {
                         $hash = Hash::unique();
                         $hashCheck = $this->_db->get('users_session', array('user_id', '=', $this->data()->id)); 
                         
-                        if(!$hashCheck->count()) {
+                        if(!$hashCheck->count())
+                        {
                             $this->_db->insert('users_session', array(
                                 'user_id' => $this->data()->id,
                                 'hash' => $hash
                             ));
-                        } else {
+                        } 
+                        else 
+                        {
                             $hash = $hashCheck->first()->hash;
                         }
 
@@ -76,11 +100,13 @@ class User {
         return false;
     }
 
-    public function exists() {
+    public function exists() 
+    {
         return (!empty($this->_data)) ? true : false;
     }
 
-    public function logout() {
+    public function logout() 
+    {
 
         $this->_db->delete('users_session', array('user_id', '=', $this->data()->id));
 
@@ -90,11 +116,13 @@ class User {
 
     }
 
-    public function data() {
+    public function data() 
+    {
         return $this->_data;
     }
 
-    public function isLoggedIn() {
+    public function isLoggedIn() 
+    {
         return $this->_isLoggedIn;
     }
 }
