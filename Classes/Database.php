b4d44828d842b02ebed252c2b26a0b61b886c976
nicholas harper <nicholasharper000@gmail.com>
2024-12-15 07:53:15 -0800

    Update Database.php

--- Classes/Database.php
+++ Classes/Database.php
@@ -1,11 +1,16 @@
 <?php
-class Database {
-   protected function connect() {
-       try {
+class Database 
+{
+   protected function connect() 
+   {
+       try
+       {
            $conn = new PDO("mysql:host=localhost;dbname=movies_database", 'root', '');
            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
            return $conn;
-       } catch(PDOException $e) {
+       } 
+       catch(PDOException $e) 
+       {
            echo "Connection failed: " . $e->getMessage();
        }
    }
