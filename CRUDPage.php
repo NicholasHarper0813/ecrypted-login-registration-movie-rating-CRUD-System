a64682a491352c57cc5edb0ecdf0fe6b3cf37694
nicholas harper <nicholasharper000@gmail.com>
2024-12-20 18:07:21 -0800

    Update CRUDPage.php

--- CRUDPage.php
+++ CRUDPage.php
@@ -5,24 +5,17 @@ require_once 'Classes/Database.php';
 
 if(isset($_GET['del'])) {
     $id = $_GET['del'];
-
     $movie = new Movie();
     $movie->destroy($id);
 }
-
-
 ?>
 
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
     <title>Movies</title>
   </head>
   <body>
@@ -48,12 +41,9 @@ if(isset($_GET['del'])) {
             </thead>
             <tbody>
                 <?php
-                   $movie = new Movie();
-                   
+                   $movie = new Movie();              
                    $rows = $movie->select();
-
                    foreach($rows as $row) {
-
                 ?>
                     <tr>
                         <td><?php echo $row['title']; ?></td>
@@ -63,17 +53,11 @@ if(isset($_GET['del'])) {
                             <a class="btn btn-sm btn-danger" href="CRUDPage.php?del=<?php echo $row['id']; ?>">Delete</a></td>
                     </tr>
                 <?php
-
                    }
-
-                ?>
-            
+                ?> 
             </tbody>
         </table>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
